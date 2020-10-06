e5f190f2dc887ad7a658ab127a0de59fad38a30e
imanolgam <72267503+imanolgam@users.noreply.github.com>
2020-10-05 22:11:38 -0500

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+class Sort{
+	public:
+	virtual void sort(vector<T> &data)=0;
+	void intercambiar(vector<T> &data, int posI, int posJ){
+		T temp=data[posI];
+		data[posI]=data[posJ];
+		data[posJ]=temp;
+	}
+	
+	void imprimir(vector<T> data){
+		for(int i=0; i<data.size(); i++){
+			cout<<data[i]<<",";
+		}
+		cout<<endl;
+	}
+};
+
+template <class T>
+class LuckySort: public Sort<T>{
+	public:
+	void sort(vector<T> &data){
+		this->intercambiar(data, 0, data.size()-1);
+	}
+};
+
+template <class T>
+class SelectionSort: public Sort<T>{
+	public:
+	void sort(vector<T> &data){
+		for(int i=0; i<data.size()-1; i++){
+			int m=i;
+			for(int j=i+1; j<data.size(); j++){
+				if(data[j]<data[m]){
+					m=j;
+				}
+			}
+			this->intercambiar(data, i, m);
+		}
+	}
+};
+
+template <class T>
+class BubbleSort: public Sort<T>{
+	public:
+	void sort(vector<T> &data){
+		for(int i=0; i<data.size()-1; i++){
+			for(int j=0; j<data.size()-1-i; j++){
+				if(data[j]>data[j+1]){
+					this->intercambiar(data, j, j+1);
+				}
+			}
+		}
+	}
+};
+
+template <class T>
+class InsertionSort: public Sort<T>{
+	public:
+	void sort(vector<T> &data){
+		for(int i=1; i<data.size(); i++){
+			for(int j=i; j>0; j--){
+				if(data[j]<data[j-1]){
+					this->intercambiar(data, j, j-1);
+				}else{
+					break;
+				}
+			}			
+		}
+	}
+};
+
+template <class T>
+class MergeSort: public Sort<T>{
+	
+	public:
+	void sort(vector<T> &data){
+		sortAux(data, 0, data.size()-1);
+	}
+	private:
+	void sortAux(vector<T> &data, int lo, int hi){
+		if(lo>=hi){
+			return;
+		}
+		int mid=(lo+hi)/2;
+		sortAux(data, lo, mid);
+		sortAux(data, mid+1, hi);
+		merge(data, lo, mid, hi);
+	}
+	
+	
+	
+	void merge(vector<T> &data, int low, int mid, int hi){
+	int sizeA=mid-low+1;
+	int sizeB=hi-mid;
+	vector<T> copiaA;
+	vector<T> copiaB;
+	
+	for(int i=0; i<sizeA; i++){
+		copiaA.push_back(data[low+i]);
+	}
+	
+	for(int i=0; i<sizeB; i++){
+		copiaB.push_back(data[mid+i+1]);
+	}
+	
+	int indexA=0;
+	int indexB=0;
+	int indexData=low;
+	while(indexA<sizeA&&indexB<sizeB){
+		if(copiaA[indexA]<copiaB[indexB]){
+			data[indexData]=copiaA[indexA];
+			indexA++;
+		}else{
+			data[indexData]=copiaB[indexB];
+			indexB++;
+		}
+		indexData++;
+	}
+	
+	while(indexA<sizeA){
+		data[indexData]=copiaA[indexA];
+		indexA++;
+		indexData++;
+	}
+	
+	while(indexB<sizeB){
+		data[indexData]=copiaB[indexB];
+		indexB++;
+		indexData++;
+	}
+}
+	
+};
+
+template <class T>
+class QuickSort: public Sort<T>{
+	public:
+	void sort(vector<T> &data){
+		sortAux(data, 0, data.size()-1);
+	}
+	
+	void sortAux(vector<T> &data, int low, int hi){
+		if(low>=hi){
+			return;
+		}
+		int j=partition(data, low, hi);
+		sortAux(data, low, j-1);
+		sortAux(data, j+1, hi);
+		
+	}
+	
+	int partition(vector<T> &data, int low, int hi){
+		int pivote=low;
+		int i=low+1;
+		int j=hi;
+		while(true){
+			while(data[i]<=data[pivote]&& i<hi){
+				i++;
+			}
+			while(data[j]>data[pivote]&& j>low){
+				j--;
+			}
+			if (i>=j){
+				break;
+			}else{
+				this->intercambiar(data, i, j);
+			}
+		}
+		this->intercambiar(data, pivote, j);
+		return j;
+	}
+	
+};
+
+
+int main(){
+	
+	MergeSort<int> s;
+	vector<int> data;
+    data.push_back(2);
+    data.push_back(7);
+    data.push_back(5);
+    data.push_back(1); 
+	s.sort(data);
+	s.imprimir(data);
+	return 0;
+}
